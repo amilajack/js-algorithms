28d9bd132d842823531ef7de205e271976e12043
Amila Welihinda <amilajack@gmail.com>
2016-03-21 22:37:47 -0700

    Added tasks to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,26 @@ import gulp from 'gulp'
 import del from 'del'
 import babel from 'gulp-babel'
 
-const scripts = ['**/*']
-const distPath = ['./dist']
+const scripts = [
+  'LeetCode/*',
+  'Math/*',
+  'Reverse/*'
+];
+const distPath = './dist'
 
 // Remove all compiled scripts
-gulp.task('clean', cb => del([distPath], { dot: true }))
+gulp.task('clean', cb => del(['./dist'], { dot: true }))
+
+// Watch scripts
+gulp.task('watch', () => {
+  gulp.watch(scripts, ['default']);
+})
 
 // Default task
 gulp.task('default', ['clean'], () => {
 
   gulp
     .src(scripts)
-    .pipe(babel(scripts))
+    .pipe(babel())
     .pipe(gulp.dest(distPath));
 })
