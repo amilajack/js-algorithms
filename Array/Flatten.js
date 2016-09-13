ac8e378dbff024a8df7bf33fc5ea71f130a4764c
Amila Welihinda <amilajack@gmail.com>
2016-09-13 09:05:08 -0700

    Added initial arrray flatte

---
+++ Array/Flatten.js
@@ -0,0 +1,32 @@
+/**
+ * Take a two dimentional array (array of arrays) and 'flatten' it. In other
+ * words, this means to take multiple arrays and merge them together
+ *
+ * Javascript's Array.prototype.concat would make this easier but it creates a
+ * new array every single time
+ *
+ * Complexity: O(n^2)
+ */
+const expect = require('chai').expect
+
+
+function Flatten(twoDimentionalArray, collector = []) {
+  switch (twoDimentionalArray.length > 0) {
+    case true:
+      // ES5
+      // const first = twoDimentionalArray[0];
+      // const rest = twoDimentionalArray.splice(1);
+      const [first, ...rest] = twoDimentionalArray
+      // ES5
+      // return Flatten(rest, first.concat(collector))
+      return Flatten(rest, [...first, ...collector])
+    default:
+      return collector
+  }
+}
+
+expect(
+  Flatten([[1, 3, 4, 5], [13, 15, 41, 54,]])
+    .sort((a, b) => (a < b))
+)
+  .to.eql([1, 3, 4, 5, 13, 15, 41, 54])
--- Sorting/MiddleShift.js
+++ Sorting/MiddleShift.js
@@ -3,31 +3,35 @@
  *
  * Solution: Find the index closest to the 'middle' of an array. Then add the
  *           elements that follow it to separate
- *
- * Complexity: O((n/2)^2)
  */
 const expect = require('chai').expect
 
 
+/**
+ * Complexity: O((n/2)^2)
+ */
 function SlowMiddleShift(string, char) {
-  const round = Math.floor(string.length / 2)
-  let formatted = string.substring(0, round)
+  const middleIndex = Math.floor(string.length / 2)
+  let formatted = string.substring(0, middleIndex)
 
   // BAD
   // O((n/2)^2)
   formatted += char
 
-  for (let i = round; i < string.length; i++) {
+  for (let i = middleIndex; i < string.length; i++) {
     formatted += string[i];
   }
 
   return formatted
 }
 
+/**
+ * Complexity: O(n+1)
+ */
 function FastMiddleShift(string, char) {
-  const round = Math.floor(string.length / 2)
-
-  
+  const middleIndex = Math.floor(string.length / 2)
+  return string.substring(0, middleIndex) + char + string.substring(middleIndex)
 }
 
 expect(SlowMiddleShift('some', 'e')).to.equal('soeme')
+expect(FastMiddleShift('some', 'e')).to.equal('soeme')
