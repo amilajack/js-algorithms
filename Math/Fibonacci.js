8226c458e6955e3b066760ad6ebb63545edf59a8
Amila Welihinda <amilajack@gmail.com>
2016-11-18 22:24:44 -0800

    Added Pascal solution

--- Math/Fibonacci.js
+++ Math/Fibonacci.js
@@ -24,6 +24,6 @@ export default function Fibonacci(number: num): num[] {
   })()
 }
 
-test('Fibonacci()()', () => {
+test('Fibonacci()', () => {
   expect(Fibonacci(10)).to.eql([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
 })
--- Math/Pascal.js
+++ Math/Pascal.js
@@ -4,51 +4,58 @@
 //
 // For example, given numRows = 5, return
 // [
-//      [1],
-//     [1,1],
-//    [1,2,1],
-//   [1,3,3,1],
-//  [1,4,6,4,1],
-//  [1,5,10,5,1]
+//       [1],
+//      [1,1],
+//     [1,2,1],
+//    [1,3,3,1],
+//   [1,4,6,4,1],
+//  [1,5,10,10,5,1]
 // ]
 //
 // @flow
 import { expect } from 'chai'
 
 
-type pt = Array<Array<any>>
 type num = number
+type pt = Array<Array<number>>
 
 function PascalRecursive(number: num, list: pt = []): pt {
   switch (list.length) {
     case 0:
       list.push([1])
-      return list
+      return PascalRecursive(
+        number,
+        list
+      )
     case 1:
-      list.push([1], [1])
-      return list
+      list.push([1, 1])
+      return PascalRecursive(
+        number,
+        list
+      )
     case number:
       return list
     default: {
       const _list = list[list.length - 1]
-      _list.splice(1, _list.length - 1)
+      const _tmp = [1]
+      for (let i = 0; i < _list.length - 1; i++) {
+        _tmp.push(_list[i] + _list[i + 1])
+      }
+      _tmp.push(1)
+      list.push(_tmp)
       return PascalRecursive(
         number,
-        _list
+        list
       )
     }
   }
 }
 
-// function PascalIterative(number: number) {
-//   const pascalNumbers = []
-//   for (let i = 0; i < number; i++) {
-//     pascalNumbers.push()
-//   }
-// }
+// @TODO
+// function PascalIterative(number: number) {}
 
-
-expect(PascalRecursive(1)).to.eql([
+// Tests
+expect(PascalRecursive(2)).to.eql([
   [1],
   [1, 1]
 ])
