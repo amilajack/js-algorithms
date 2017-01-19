a56c61f7a692227fba9985a8307e73fd928d4282
John Tran <j-tran@users.noreply.github.com>
2017-01-18 23:13:20 -0800

    Added Insertion Sort, and some comments to other files (#17)
    * Added QuickSelect.js
    
    * Added QuickSelect algorithm and changed readme.md
    
    * Code style changes
    
    * Added Quicksort and clarified variable names
    
    * Formatted Quicksort
    
    * Added InsertionSort and comments for other algorithms
    
    * Update InsertionSort.js
    
    * Update MergeSort.js
    
    * Update QuickSort.js
    
    * Update InsertionSort.js

--- Sorting/QuickSort.js
+++ Sorting/QuickSort.js
@@ -1,14 +1,27 @@
-// @flow
+/**
+ * QuickSort is an efficient sorting algorithm that utilizes the divide-and-conquer paradigm
+ *
+ * Notes:
+ * - Algorithm derived via Introduction to Algorithm (Cormen et al)
+ * - Has an optimal best and average case (O(nlgn)) but unlikely poor worst case (O(n^2))
+ *   - Worst occurs when QuickSort produces a maximally unbalanced subproblem with n-1 elements
+ *     and one with 0 elements, and happens through every iteration of QuickSort
+ * - Not stable (Other implementations of QuickSort are)
+ * - In-place (Other implementations of QuickSort aren't)
+ * - This implementation uses randomly selected pivots for better performance
+ *
+ * @flow
+ */
 import { expect } from 'chai'
 
 
-export default function QuickSort(items: number[]): number[] {
+export default function QuickSort(items: Array<number>): Array<number> {
   const itemsCopy = [...items]
   QuickSortRcrs(itemsCopy, 0, itemsCopy.length - 1)
   return itemsCopy
 }
 
-function QuickSortRcrs(items: number[], p: number, r: number) {
+function QuickSortRcrs(items: Array<number>, p: number, r: number) {
   if (p < r) {
     const q = RandomizedPartition(items, p, r)
     QuickSortRcrs(items, p, q - 1)
@@ -16,13 +29,13 @@ function QuickSortRcrs(items: number[], p: number, r: number) {
   }
 }
 
-function RandomizedPartition(items: number[], p: number, r: number): number {
+function RandomizedPartition(items: Array<number>, p: number, r: number): number {
   const i = getRandomInt(p, r)
   Swap(items, i, r)
   return Partition(items, p, r)
 }
 
-function Partition(items: number[], p: number, r: number): number {
+function Partition(items: Array<number>, p: number, r: number): number {
   const x = items[r]
   let i = p - 1
   for (let j = p; j < r; j++) {
@@ -39,7 +52,7 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function Swap(arr: any[], x: any, y: any) {
+function Swap(arr: Array<any>, x: any, y: any) {
   const temp = arr[x]
   arr[x] = arr[y]
   arr[y] = temp
