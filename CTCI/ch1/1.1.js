fe44895f441ce753c9c24856005eecf8b92ae284
Amila Welihinda <amilajack@gmail.com>
2017-06-10 10:43:05 -0700

    Added reverse method

---
+++ CTCI/ch1/1.1.js
@@ -0,0 +1,22 @@
+/**
+ * Check if a string has all unique characters
+ *
+ * If this needs to be done in place, use an in-place sorting algorithm
+ * on the string and the check for each char if the next char is equal to
+ * the current char. If it is, return false
+ *
+ * @flow
+ */
+export default function CheckHashAllUniqueChars(string: string): bool {
+  const set = new Set();
+
+  for (const char of string) {
+    if (!set.has(char)) {
+      set.add(char);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
---
+++ General/PrintKDistance.js
@@ -0,0 +1,53 @@
+// Given a string S, and an integer K, rearrange the string such that
+// similar characters are at least K distance apart.
+
+// Example:
+
+// S = AAABBBCC, K = 3
+// Result : ABCABCABC (all 'A's are 3 distance apart, similarly with
+// B's and C's)
+
+// S = AAABC, K=2 : Not possible. (EDIT : k=3 is not possible).
+
+// S = AAADBBCC, K = 2:
+// Result: ABCABCDA
+
+function PrintKDistance(string) {
+  const chars = Array.from(string);
+  const map = new Map();
+
+  for (const char of chars) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+
+  let str = '';
+
+  const itr = 1000;
+  let i = 0;
+
+  while (map.size > 0) {
+    for (const key of map.keys()) {
+      str += key;
+      // Decrement key
+      map.set(key, map.get(key) - 1);
+      // If key = 0, remove
+      if (map.get(key) === 0) {
+        map.delete(key);
+      }
+    }
+    if (i > itr) {
+      break;
+    }
+    i++;
+  }
+
+  return str;
+}
+
+
+console.log(PrintKDistance('AAABBBCC'));
+
