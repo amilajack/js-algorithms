c54d837e96aed7f9cc816274ead517d0abb44a9e
John Tran <jptran318@gmail.com>
2017-09-21 12:26:34 -0700

    Added 1.2 and 1.4 from CTCI

---
+++ CTCI/ch1/1.4.js
@@ -0,0 +1,35 @@
+// @flow
+
+/**
+ *
+ * O(N) time
+ * O(N) space
+ * @param {*} str the string to check
+ * @return true if str is a permutaiton of a palindrome
+ */
+export default function palindromePermutation(str: string) {
+  // create new string that ignores whitespace
+  // for each character, use it as a key and icrment the value in a hash table
+  // have a flag that allows only one character to appear once
+  // check if all the values in the hash table === 2 or if at most one === 1
+  const newStr = str.replace(/ /g, '').toLowerCase();
+  const charCount = new Map();
+  for (let i = 0; i < newStr.length; i++) {
+    if (!charCount.get(newStr[i])) {
+      charCount.set(newStr[i], 1);
+    } else {
+      charCount.set(newStr[i], charCount.get(newStr[i]) + 1);
+    }
+  }
+
+  let flagCount = 0;
+  for (let i = 0; i < newStr.length; i++) {
+    if (charCount.get(newStr[i]) === 1) {
+      flagCount += 1;
+    }
+    if (flagCount === 2) {
+      return false;
+    }
+  }
+  return true;
+}
