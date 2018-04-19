b9f1594b5040d297017ba19ad9f68df893b05098
vikr01 <28772991+vikr01@users.noreply.github.com>
2018-04-18 21:57:46 -0700

    added 4.3 v1 (#61)

---
+++ CTCI/ch4/4.3.js
@@ -0,0 +1,64 @@
+/*
+	List of Depths: Given a binary tree, design an algorithm which creates
+	a linked list of all the nodes at each depth (e.g., if you have a tree with depth 0,
+	you'll have 0 linked lists).
+ */
+
+export default function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+Node.prototype.insert = function(value) {
+  if (value < this.value) {
+    if (!this.left) {
+      return (this.left = new Node(value));
+    }
+    return this.left.insert(value);
+  }
+
+  return this.right ? this.right.insert(value) : (this.right = new Node(value));
+};
+
+Node.prototype.print = function() {
+  const leftstr = this.left ? `${this.left.print()}, ` : '';
+  return leftstr + this.value + (this.right ? `, ${this.right.print()}` : '');
+};
+
+const root = new Node(0); // start with a node at 0
+
+//for (let i = 0; i < 10; i++) {
+//  root.insert(Math.floor(Math.random() * (20 - -20)) - 20);
+//}
+//
+//console.log(root.print()); // print the tree
+
+Node.prototype.listify = function() {
+  const lists = [[this.value]];
+  let nextQueue = [this.right, this.left],
+    queue;
+
+  while (nextQueue.length !== 0) {
+    queue = nextQueue;
+    nextQueue = [];
+    const newlist = [];
+    while (queue.length !== 0) {
+      const node = queue.pop();
+      if (node) {
+        if (node.left) {
+          nextQueue.unshift(node.left);
+        }
+        if (node.right) {
+          nextQueue.unshift(node.right);
+        }
+        newlist.push(node.value);
+      }
+    }
+    lists.push(newlist);
+  }
+
+  return lists;
+};
+
+console.log(root.listify());
--- test/CTCI.spec.js
+++ test/CTCI.spec.js
@@ -1,4 +1,5 @@
 import CheckHashAllUniqueChars from '../CTCI/ch1/1.1';
+import TreeDepthLists from '../CTCI/ch4/4.3';
 
 describe('CTCI', () => {
   describe('Ch. 1.1', () => {
@@ -6,6 +7,33 @@ describe('CTCI', () => {
       expect(CheckHashAllUniqueChars('foo')).toEqual(false);
       expect(CheckHashAllUniqueChars('fo')).toEqual(true);
       expect(CheckHashAllUniqueChars('car')).toEqual(true);
+    });
+  });
+
+  describe('Ch. 4.3', () => {
+    it('depth=listNumber', () => {
+      const tree = new TreeDepthLists(0);
+      tree.insert(3);
+      tree.insert(5);
+
+      //0
+      // \
+      //  3
+      //   \
+      //    5
+
+      expect(tree.listify()).toEqual([[0], [3], [5]]);
+
+      tree.insert(-5);
+      tree.insert(-3);
+
+      //        0
+      //      /  \
+      //     -5    3
+      //       \    \
+      //        -3   5
+
+      expect(tree.listify()).toEqual([[0], [-5, 3], [-3, 5]]);
     });
   });
 });
