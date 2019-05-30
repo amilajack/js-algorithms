f826f54f3cfca1680232f7fbd3097aad1f4f3cf9
Amila Welihinda <amilajack@gmail.com>
2019-05-30 16:01:48 -0700

    add 257 leetcode problem

---
+++ LeetCode/Easy/257.js
@@ -0,0 +1,19 @@
+// See https://leetcode.com/problems/binary-tree-paths/
+
+const getPaths = (root, paths, parent) => {
+  if (root === null) return;
+  // Clone
+  parent = [...parent];
+  // Push node value
+  parent.push(root.val);
+  if (root.left) getPaths(root.left, paths, parent);
+  if (root.right) getPaths(root.right, paths, parent);
+  if (!root.left && !root.right) paths.push(parent.join('->'));
+};
+
+const binaryTreePaths = function(root) {
+  const paths = []
+  const parent = []
+  getPaths(root, paths, parent)
+  return paths;
+};
