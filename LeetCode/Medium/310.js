29c3b3a99daff931e6a6274c4676957854a4e095
Amila Welihinda <amilajack@gmail.com>
2019-06-11 16:27:35 -0700

    add comments to lc 310

--- LeetCode/Medium/310.js
+++ LeetCode/Medium/310.js
@@ -1,3 +1,19 @@
+// For an undirected graph with tree characteristics, we can choose any node
+// as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+
+// Format
+// The graph contains n nodes which are labeled from 0 to n - 1. You will be given the
+// number n and a list of undirected edges (each edge is a pair of labels).
+
+// You can assume that no duplicate edges will appear in edges. Since all edges are
+// undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+// Observations:
+// The height of a graph is maximal when the tree is rooted at a leaf. Another observation
+// is that the nodes in the centermost of the graph have the lowest height when chosen to
+// be the root. The procedure is then to delete leaves until there are are either only one
+// or two nodes left.
+
 /**
  * @param {number} n
  * @param {number[][]} edges
