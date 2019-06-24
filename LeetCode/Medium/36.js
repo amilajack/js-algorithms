8db850296efaeeb7b35bc1ab7978fe58e8d72e64
Amila Welihinda <amilajack@gmail.com>
2019-06-24 11:09:07 -0700

    add lc 36

---
+++ LeetCode/Medium/36.js
@@ -0,0 +1,59 @@
+// 36. Valid Sudoku
+
+// Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+
+const isNum = n => n !== '.';
+const validateRow = (board, row) => {
+  const set = new Set();
+  for (let i = 0; i < board.length; i++) {
+    if (isNum(board[row][i]) && set.has(board[row][i])) return false;
+    set.add(board[row][i]);
+  }
+  return true;
+};
+const validateCol = (board, col) => {
+  const set = new Set();
+  for (let i = 0; i < board.length; i++) {
+    if (isNum(board[i][col]) && set.has(board[i][col])) return false;
+    set.add(board[i][col]);
+  }
+  return true;
+};
+const validateSquare = (board, i, j) => {
+  const imin = i * 3;
+  const imax = imin + 2;
+  const jmin = j * 3;
+  const jmax = jmin + 2;
+  const set = new Set();
+  for (let i = imin; i <= imax; i++) {
+    for (let j = jmin; j <= jmax; j++) {
+      if (isNum(board[i][j]) && set.has(board[i][j])) return false;
+      set.add(board[i][j]);
+    }
+  }
+  return true;
+};
+
+const isValidSudoku = function(board) {
+  for (let i = 0; i < board.length; i++) {
+    if (!validateRow(board, i)) return false;
+  }
+  for (let i = 0; i < board.length; i++) {
+    if (!validateCol(board, i)) return false;
+  }
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (!validateSquare(board, i, j)) return false;
+    }
+  }
+  return true;
+};
