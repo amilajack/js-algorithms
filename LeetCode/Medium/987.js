2170db5b4d057b907f7594052e7d11c8f55fc33f
Amila Welihinda <amilajack@gmail.com>
2019-06-29 18:41:04 -0700

    add lc 61

---
+++ LeetCode/Medium/987.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+const verticalTraversalAux = (root, map, mat) => {
+  if (!root) return;
+  const queue = [];
+  queue.push([root, 0]);
+  while (queue.length) {
+    const [node, x] = queue.shift();
+    if (map.has(x)) {
+      const a = map.get(x);
+      a.push(node.val);
+    } else {
+      map.set(x, [node.val]);
+    }
+    if (node.left) queue.push([node.left, x - 1]);
+    if (node.right) queue.push([node.right, x + 1]);
+  }
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const verticalTraversal = function(root) {
+  const map = new Map();
+  const mat = new Array().fill(new Array());
+  verticalTraversalAux(root, map, mat);
+  const keys = Array.from(map.keys()).sort((a, b) => a - b);
+  const res = [];
+  for (const key of keys) {
+    res.push(map.get(key));
+  }
+  return res;
+};
