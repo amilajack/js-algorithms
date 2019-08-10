d783fcac3ce40480cff8d21497d5e1bfaaadaef8
Amila Welihinda <amilajack@gmail.com>
2019-08-10 09:37:46 -0700

    refactor rabin karp

--- General/RabinKarp.js
+++ General/RabinKarp.js
@@ -10,25 +10,25 @@
 // m = "aba"
 // [0, 2]
 
-function hashFn() {
-  let hash = 0;
+export function hash() {
+  let hashedValue = 0;
   let i;
   let chr;
-  if (this.length === 0) return hash;
+  if (this.length === 0) return hashedValue;
   for (i = 0; i < this.length; i++) {
     chr = this.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
+    hashedValue = (hashedValue << 5) - hashedValue + chr;
+    hashedValue |= 0; // Convert to 32bit integer
   }
-  return hash;
+  return hashedValue;
 }
 
 export default function RabinKarp(m, n) {
   const indicies = [];
-  const hash = hashFn(n);
+  const hashedValue = hash(n);
   for (let i = 0; i < m.length; i++) {
     const str = m.splice(i, i + n.length);
-    if (hashFn(str) === hash) {
+    if (hash(str) === hashedValue) {
       indicies.push(i);
     }
   }
