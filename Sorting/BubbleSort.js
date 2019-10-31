7df30c1823668fc66e4d0478a4c5da46be0094d0
Sherlyn(Shao Juan) <31661914+rollingcomma@users.noreply.github.com>
2019-10-31 11:27:29 -0700

    Added BubbleSort (#123)
    * adding BubbleSort
    
    * updated BubbleSort.js style

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,25 @@
+/**
+ * For each element, comparing with adjacent element
+ * If it is bigger, swap.
+ *
+ * @complexity: O(n^2)
+ * @flow
+ */
+export default function BubbleSort(numberArray: number[]): number[] {
+  let { length } = numberArray;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < length - 1; i++) {
+      if (numberArray[i] > numberArray[i + 1]) {
+        const temp = numberArray[i];
+        numberArray[i] = numberArray[i + 1];
+        numberArray[i + 1] = temp;
+        swapped = true;
+      }
+    }
+    // after each round of inside loop, the largest element will fall to the bottom of the array.
+    length -= 1; 
+  } while (swapped);
+  return numberArray;
+}
