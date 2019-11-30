1031d6287a79d15f68df19a663da5d123f54e98f
Amila Welihinda <amilajack@gmail.com>
2019-11-29 20:18:47 -0800

    add rate limit problem

---
+++ General/RateLimit.js
@@ -0,0 +1,44 @@
+export function rateLimitV1(fn, ms) {
+  let isRL = false;
+  return function ratelimit(...args) {
+    if (isRL) return;
+    isRL = true;
+    setTimeout(() => {
+      isRL = false;
+    }, ms);
+    return fn(...args);
+  };
+}
+
+export default function rateLimit(fn, ms) {
+  let isRL = false;
+  const argsQueue = [];
+
+  function takeFromQueue() {
+    setTimeout(() => {
+      if (argsQueue.length) {
+        const _args = argsQueue.shift();
+        fn(..._args);
+        takeFromQueue();
+      } else {
+        isRL = false;
+      }
+    }, ms);
+  }
+
+  return function _ratelimit(...args) {
+    if (isRL) {
+      argsQueue.push(args);
+      return;
+    }
+    fn(...args);
+    isRL = true;
+    takeFromQueue();
+  };
+}
+
+const logRl = rateLimit(console.log, 2000);
+
+logRl(1000);
+logRl(100);
+logRl(100);
