57f3fa1de60f191da48c38ec4f2a59dc8b86bbf2
Nour B <56294154+nourrrrrrrr@users.noreply.github.com>
2020-04-27 13:25:08 +0100

    Update DijkstrasSmallestPath.js

--- Math/DijkstrasSmallestPath.js
+++ Math/DijkstrasSmallestPath.js
@@ -1,139 +1,102 @@
-class PriorityQueue {
-    constructor() {
-        this.vertices = [];
-    }
+function solve(graph, s) {
+  var solution = {};
+  solutions[s] = [];
+  solutions[s].dist = 0;
 
-    /* Each node added will be an object with node-value and weight.
-       Iterate through the verices and insert node before the first element found that has a larger weight than the node.
-       lower weight = higher priority
-    */
-    enqueue(element,priority) {
-        if(this.isEmpty()) {
-            this.vertices.push({value:element, weight: priority});
-        } else {
-            let added = false;
-            for(let i=1; i<= this.vertices.length; i++) {
-                if( priority < this.vertices[i-1]["weight"]) {
-                    this.vertices.splice(i-1,0,{value:element, weight: priority});
-                    added = true;
-                    break;
-                }
-            }
-            if(!added) {
-                this.vertices.push({value:element, weight: priority});
-            }
+  while(true) {
+    var parent = null;
+    var nearest = null;
+    var dist = Infinity;
+
+
+    for(var n in solutions) {
+      if(!solutions[n])
+        continue
+      var ndist = solutions[n].dist;
+      var adj = graph[n];
+      //for each of its adjacent nodes...
+      for(var a in adj) {
+        //without a solution already...
+        if(solutions[a])
+          continue;
+        //choose nearest node with lowest *total* cost
+        var d = adj[a] + ndist;
+        if(d < dist) {
+          //reference parent
+          parent = solutions[n];
+          nearest = a;
+          dist = d;
         }
-    }
-
-    // Removes first element from priority queue
-    dequeue() {
-        let node = this.vertices.shift();
-        return node;
-    }
-
-    // Helper method to check if queue is empty
-    isEmpty() {
-        return this.vertices.length === 0;
-    }
-}
-
-class Node {
-    constructor(value,weight) {
-        value = this.value;
-        weight = this.weight;
-    }
-}
-
-class Graph {
-    constructor() {
-        this.nodes = [];
-        this.adjacencyList = {};
       }
-
-    addVertex(vertex) {
-        this.nodes.push(vertex);
-        this.adjacencyList[vertex] = [];
-        return vertex;
     }
 
-    addEdge(vertex1, vertex2, weight) {
-        if(!this.nodes.includes(vertex1)) {
-            this.addVertex(vertex1);
-        }
-        if(! this.nodes.includes(vertex2)) {
-            this.addVertex(vertex2);
-        }
 
-        this.adjacencyList[vertex1].push({value: vertex2, weight: weight});
-        this.adjacencyList[vertex2].push({value: vertex1, weight: weight});
+    if(dist === Infinity) {
+        break;
     }
 
-    dijkastraAlgo(startNode) {
-        let distances = {};
-        let previous = {}; //ref to previous node
-        let pq = new PriorityQueue();
+    solutions[nearest] = parent.concat(nearest);
 
-        /* Set distances to all nodes except start to infinite */
-        distances[startNode] = 0;
-        pq.enqueue(startNode,0);
-        this.nodes.forEach(node => {
-            if( node !== startNode) {
-                distances[node] = Infinity;
-            }
-            previous[node] = null;
-        });
+    solutions[nearest].dist = dist;
+  }
 
-
-        while(!pq.isEmpty()) {
-            let minnode = pq.dequeue();
-            let currentNode = minnode.value;
-            let currentWeight = minnode.weight;
-
-            this.adjacencyList[currentNode].forEach(neighbour => {
-                let tempWeight = distances[currentNode] + neighbour["weight"];
-                if(tempWeight< distances[neighbour.value]) {
-                    distances[neighbour.value] = tempWeight;
-                    previous[neighbour.value] = currentNode;
-                    pq.enqueue(neighbour.value,distances[neighbour.value]);
-                }
-            });
-        }
-        return distances;
-    }
+  return solutions;
 }
 
+var graph = {};
 
-let graph = new Graph();
-graph.addEdge("A","C",3);
-graph.addEdge("A","B",7);
-graph.addVertex("D");
-graph.addEdge("C","D",2);
-graph.addEdge("D","B",2);
-graph.addEdge("B","E",6);
-graph.addEdge("D","E",4);
-graph.addEdge("C","B",1);
+\\create graph
 
-graph.dijkastraAlgo("A");
-//{A: 0, C: 3, B: 4, D: 5, E: 9}
+var layout = {
+  'R': ['2'],
+  '2': ['3','4'],
+  '3': ['4','6','13'],
+  '4': ['5','8'],
+  '5': ['7','11'],
+  '6': ['13','15'],
+  '7': ['10'],
+  '8': ['11','13'],
+  '9': ['14'],
+  '10': [],
+  '11': ['12'],
+  '12': [],
+  '13': ['14'],
+  '14': [],
+  '15': []
+}
 
+//convert uni-directional to bi-directional graph
+// needs to look like: where: { a: { b: cost of a->b }
+// var graph = {
+//     a: {e:1, b:1, g:3},
+//     b: {a:1, c:1},
+//     c: {b:1, d:1},
+//     d: {c:1, e:1},
+//     e: {d:1, a:1},
+//     f: {g:1, h:1},
+//     g: {a:3, f:1},
+//     h: {f:1}
+// };
 
+for(var id in layout) {
+  if(!graph[id])
+    graph[id] = {};
+  layout[id].forEach(function(aid) {
+    graph[id][aid] = 1;
+    if(!graph[aid])
+      graph[aid] = {};
+    graph[aid][id] = 1;
+  });
+}
 
-let g = new Graph();
-g.addVertex("A");
-g.addVertex("B");
-g.addVertex("C");
-g.addVertex("D");
-g.addVertex("E");
-g.addVertex("F");
-g.addVertex("G");
+//choose start node
+var start = '10';
+//get all solutions
+var solutions = solve(graph, start);
 
-g.addEdge("A", "C", 100);
-g.addEdge("A", "B", 3);
-g.addEdge("A", "D", 4);
-g.addEdge("D", "C", 3);
-g.addEdge("D", "E", 8);
-g.addEdge("E", "F", 10);
-g.addEdge("B", "G", 9);
-g.addEdge("E", "G", 50);
-g.dijkastraAlgo("A");
-//{ A: 0, B: 3, C: 7, D: 4, E: 12, F: 22, G: 12 }
+console.log("From '"+start+"' to");
+//display solutions
+for(var s in solutions) {
+  if(!solutions[s]) continue;
+  console.log(" -> " + s + ": [" + solutions[s].join(", ") + "]   (dist:" + solutions[s].dist + ")");
+}
