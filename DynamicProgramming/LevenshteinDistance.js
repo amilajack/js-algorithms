2f0c120122073cca4302208e011aec9c1cdf6837
Muhammad Bintang Bahy <bahybintang@gmail.com>
2020-08-01 22:26:31 +0700

    fix linter checks errors

--- DynamicProgramming/LevenshteinDistance.js
+++ DynamicProgramming/LevenshteinDistance.js
@@ -2,7 +2,7 @@
 // Levenshtein distance may also be referred to as edit distance
 
 function getEmptyArray(N, M) {
-  let arr = new Array(N);
+  const arr = new Array(N);
   for (let i = 0; i < N; i++) {
     arr[i] = new Array(M);
     for (let j = 0; j < M; j++) {
@@ -20,14 +20,14 @@ function getEmptyArray(N, M) {
  * @return {Number}      The Levenshtein distance of two input strings
  */
 export default function LevenshteinDistance(str1, str2) {
-  let dp = getEmptyArray(str1.length + 1, str2.length + 1);
+  const dp = getEmptyArray(str1.length + 1, str2.length + 1);
 
   for (let i = 0; i < str1.length + 1; i++) {
     for (let j = 0; j < str2.length + 1; j++) {
-      if (Math.min(i, j) == 0) {
+      if (Math.min(i, j) === 0) {
         dp[i][j] = Math.max(i, j);
       } else {
-        let add = str1[i - 1] != str2[j - 1] ? 1 : 0;
+        const add = str1[i - 1] !== str2[j - 1] ? 1 : 0;
         dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
         dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + add);
       }
--- test/DynamicProgramming.spec.js
+++ test/DynamicProgramming.spec.js
@@ -1,32 +1,32 @@
-import Coins from "../DynamicProgramming/Coins";
-import LevenshteinDistance from "../DynamicProgramming/LevenshteinDistance";
-import { expect } from "chai";
+import { expect } from 'chai';
+import Coins from '../DynamicProgramming/Coins';
+import LevenshteinDistance from '../DynamicProgramming/LevenshteinDistance';
 
-describe("DynamicProgramming", () => {
-  test("Coins", () => {
+describe('DynamicProgramming', () => {
+  test('Coins', () => {
     expect(Coins(10)).equal(9);
   });
 
-  test("LevenshteinDistance", () => {
-    let testCase = [
+  test('LevenshteinDistance', () => {
+    const testCase = [
       {
-        str1: "aaaaaaaaaaaaaaaaaaaaaaaXaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        str2: "aaaaaaXaaaaaaaaaaaaaaaaXaaaaaaaaaaaaaaaaaaaaXaaaaaaX",
-        ans: 3,
+        str1: 'aaaaaaaaaaaaaaaaaaaaaaaXaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        str2: 'aaaaaaXaaaaaaaaaaaaaaaaXaaaaaaaaaaaaaaaaaaaaXaaaaaaX',
+        ans: 3
       },
       {
-        str1: "aaaaaaaaaaaaaaaaaaaaaaabccccccbaaaaaaaaaaaaaaaaaaaa",
-        str2: "aaaaaaaaaaaaaaaaaaaaaaaaabccccccbaaaaaaaaaaaaaaaaaa",
-        ans: 4,
+        str1: 'aaaaaaaaaaaaaaaaaaaaaaabccccccbaaaaaaaaaaaaaaaaaaaa',
+        str2: 'aaaaaaaaaaaaaaaaaaaaaaaaabccccccbaaaaaaaaaaaaaaaaaa',
+        ans: 4
       },
       {
-        str1: "abc*efghijklm---nopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        str2: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM+++NOPQRSTU*WXYZ",
-        ans: 8,
-      },
+        str1: 'abc*efghijklm---nopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        str2: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM+++NOPQRSTU*WXYZ',
+        ans: 8
+      }
     ];
 
-    for (let test of testCase) {
+    for (const test of testCase) {
       expect(LevenshteinDistance(test.str1, test.str2)).equal(test.ans);
     }
   });
