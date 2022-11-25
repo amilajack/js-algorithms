eac8ad1655a0dbea624ec6e28dbd74ac16aab820
thesmallplanet <49563532+thesmallplanet@users.noreply.github.com>
2022-11-24 19:24:11 -0500

    added two javascript algorithms (after syncing with main branch) (#215)

---
+++ LeetCode/Easy/53.js
@@ -0,0 +1,25 @@
+// 53. Maximum Subarray
+
+// Given an integer array nums, find the subarray which has the largest sum and return its sum.
+
+// Example:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+// Follow up:
+// If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export default function maxSubArray(nums) {
+  let maxSum = 0;
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum = Math.max(sum + nums[i], nums[i]);
+    maxSum = Math.max(sum, maxSum);
+  }
+  return maxSum;
+}
---
+++ Sorting/BubbleSortRecursive.js
@@ -0,0 +1,23 @@
+/**
+ * Recursive version of the bubble sort algorithm
+ *
+ * @complexity: O(n^2)
+ * @flow
+ */
+export default function BubbleSort(arr) {
+  return _bubblesort(arr, arr.length);
+}
+
+function _bubblesort(arr, stopIndex) {
+  if (stopIndex === 1) {
+    return arr;
+  }
+  for (let i = 0; i < stopIndex - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      const temp = arr[i + 1];
+      arr[i + 1] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return _bubblesort(arr, stopIndex - 1);
+}
